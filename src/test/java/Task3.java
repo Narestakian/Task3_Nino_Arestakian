31e860aa1f5fac316363da0ca3c71687a5d13ff8
Nino Arestakian <narestakian@credo.ge>
2024-10-11 13:46:50 +0400

    Task3_Arestakian

---
+++ src/test/java/Task3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter value for x: ");
+        int x = scanner.nextInt();
+        System.out.print("Enter value for y: ");
+        int y = scanner.nextInt();
+        System.out.print("Enter operator (+, -, *, /): ");
+        String operator = scanner.next();
+        if (operator.equals("+")) {
+            System.out.println("Result: " + (x + y));
+        } else if (operator.equals("-")) {
+            System.out.println("Result: " + (x - y));
+        } else if (operator.equals("*")) {
+            System.out.println("Result: " + (x * y));
+        } else if (operator.equals("/")) {
+            if (x != 0 && y != 0) {
+                System.out.println("Result: " + (x / y));
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
+            }
+        } else {
+            System.out.println("Error: Invalid operator. Please use +, -, *, or /.");
+        }
+        }
+    }
+
+
