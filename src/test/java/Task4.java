01340821b8d56bc4a61879f4a4c523216da47c1d
Nino Arestakian <narestakian@credo.ge>
2024-10-17 23:00:58 +0400

    Task4_Arestakian

--- src/test/java/Task4.java
+++ src/test/java/Task4.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Task4 {
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Enter positive number: ");
+        int number = scaner.nextInt();
+        int sum = 0;
+        int i = 2;
+        if (number>0){
+            while(i<=number){
+                sum +=i;
+                i+=2;
+            }
+            System.out.println("Summery of even numbers is: "+sum);
+        }
+        else{
+            System.out.println("Enter only positive number");
+        }
+    }
 }
